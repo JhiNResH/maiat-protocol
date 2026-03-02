e62776394aa8dfc2227415ab895fff19be7f2e47
JhiNResH <dontforgetbananatiff@gmail.com>
2026-03-02 13:12:41 -0800

    chore: add ESLint flat config (0 errors, 109 warnings)

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import nextPlugin from "@next/eslint-plugin-next";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  {
+    files: ["src/**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    plugins: {
+      "@next/next": nextPlugin,
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
+  {
+    ignores: ["contracts/**", "node_modules/**", ".next/**"],
+  },
+];
